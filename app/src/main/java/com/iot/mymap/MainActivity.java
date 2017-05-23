eb84b84176e65348abd69f8ef61d51d50811e185
soo <soo>
2017-05-23 15:25:20 +0900

    Initial commit

---
+++ app/src/main/java/com/iot/mymap/MainActivity.java
@@ -0,0 +1,35 @@
+package com.iot.mymap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnMap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        Button button2 = (Button) findViewById(R.id.btnCheck);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(MainActivity.this,ProximityActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+}
---
+++ app/src/main/java/com/iot/mymap/MapActivity.java
@@ -0,0 +1,173 @@
+package com.iot.mymap;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener
+{
+    private static final String TAG = "MainActivity";
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+
+    final static double mLatitude = 37.541697;   //위도
+    final static double mLongitude = 126.840417;  //경도
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        try {
+            MapsInitializer.initialize(this);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        Button button = (Button) findViewById(R.id.btnCurrentLocation);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestMyLocation();
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "GoogleMap is ready.");
+        this.map = googleMap;
+
+        //지도타입 - 일반
+        this.map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //기본위치
+        LatLng position = new LatLng(mLatitude , mLongitude);
+
+        //화면중앙의 위치와 카메라 줌비율
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 14));
+
+        map.setOnMapClickListener(this);
+    }
+
+    public void onAddMarker(double _latitude, double _longitude){
+        LatLng position = new LatLng(_latitude , _longitude);  //마커의 위치
+
+        MarkerOptions mymarker = new MarkerOptions()
+                .icon(BitmapDescriptorFactory.defaultMarker(300f))  //마커색상지정
+                .title("NO")
+                .position(position)
+                .alpha(0.7f);   //마커위치
+
+        //마커추가
+        this.map.addMarker(mymarker);
+    }
+
+    public void onMapClick(LatLng point){
+        Point screenPt=map.getProjection().toScreenLocation(point);
+        LatLng latLng = map.getProjection().fromScreenLocation(screenPt);
+
+        onAddMarker(point.latitude,point.longitude);
+        Log.d("맵좌표","좌표: 위도(" + String.valueOf(point.latitude) + "), 경도(" + String.valueOf(point.longitude) + ")");
+        Log.d("화면좌표","화면좌표: X(" + String.valueOf(screenPt.x) + "), Y(" + String.valueOf(screenPt.y) + ")");
+    }
+
+    private void requestMyLocation() {
+        LocationManager manager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            long minTime = 10000;
+            float minDistance = 0;
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime,
+                    minDistance,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            showCurrentLocation(location);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    }
+            );
+
+            Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastLocation != null) {
+                showCurrentLocation(lastLocation);
+            }
+
+            manager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    minTime,
+                    minDistance,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            showCurrentLocation(location);
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    }
+            );
+
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showCurrentLocation(Location location) {
+        LatLng curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+    }
+
+}
