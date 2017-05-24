73f8a785d8fe6c779bd7031c57fa6c69f7d50058
강부이 <bu-yi@naver.com>
2017-05-24 11:09:11 +0900

    layout

--- app/src/main/java/com/iot/mymap/MainActivity.java
+++ app/src/main/java/com/iot/mymap/MainActivity.java
@@ -1,14 +1,21 @@
 package com.iot.mymap;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity
 {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +30,57 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        EditText distance = (EditText) findViewById(R.id.distance);
+        final CheckBox setdstnce = (CheckBox) findViewById(R.id.setdstnce);
+        final CheckBox setalarm = (CheckBox) findViewById(R.id.setalarm);
+        setalarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-//        Button button2 = (Button) findViewById(R.id.btnCheck);
-//        button2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent=new Intent(MainActivity.this,ProximityActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+                if (isChecked) {
+
+                    setdstnce.setChecked(false);
+
+                    LinearLayout container = (LinearLayout) findViewById(R.id.container);
+                    LayoutInflater inflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    inflater.inflate(R.layout.sublayout_alarm, container, true);
+
+
+                } else {
+                    LinearLayout container = (LinearLayout) findViewById(R.id.container);
+                    LayoutInflater inflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    inflater.inflate(R.layout.sublayout_alarm, container, false);
+
+                }
+            }
+        });
+        setdstnce.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    setalarm.setChecked(false);
+                    LinearLayout container = (LinearLayout) findViewById(R.id.container);
+                    LayoutInflater inflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    inflater.inflate(R.layout.sublayout_distance, container, true);
+            }else {
+                    LinearLayout container = (LinearLayout) findViewById(R.id.container);
+                    LayoutInflater inflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    inflater.inflate(R.layout.sublayout_alarm, container, false);
+
+                }
+        }});
+
+
+
+
+
+
+      //  EditText distance = (EditText) findViewById(R.id.distance);
+
+
     }
 
 }
