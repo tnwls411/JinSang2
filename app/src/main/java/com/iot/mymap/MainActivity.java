685174a6504615a8dcd55e2ee9e5af591134f19d
sang-a <kimsa94@naver.com>
2017-05-24 16:50:07 +0900

    db

--- app/src/main/java/com/iot/mymap/MainActivity.java
+++ app/src/main/java/com/iot/mymap/MainActivity.java
@@ -1,6 +1,7 @@
 package com.iot.mymap;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,28 +13,20 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
     SQLiteDatabase db;
-
-//    EditText placeInput = (EditText) findViewById(R.id.placeInput);
-//    EditText countInput = (EditText) findViewById(R.id.countInput);
-
-//    EditText distanceInput = (EditText) findViewById(R.id.distanceInput);
 //    TextView status = (TextView) findViewById(R.id.status);
-
-//    String place = placeInput.getText().toString();
-//    String count = countInput.getText().toString();
- //   String distance = distanceInput.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        createDatabase("MyMap");
-//        createTable("MyList");
+        createDatabase("MyMap");
+        createTable("MyList");
 
         Button button = (Button) findViewById(R.id.btnMap);
         button.setOnClickListener(new View.OnClickListener() {
@@ -87,61 +80,69 @@ public class MainActivity extends AppCompatActivity
         }});
 
 
-//        Button insert = (Button) findViewById(R.id.insert);
-//        insert.setOnClickListener(
-//                new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-////                        insertRecord("MyList");
-////                        executeRawQuery("MyList");
-//                    }
-//                }
-//        );
+        Button insert = (Button) findViewById(R.id.insert);
+        insert.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        insertRecord("MyList");
+//                        executeRawQuery("MyList");
+                    }
+                }
+        );
     }
 
-//    private void createDatabase(String dbName){
-//        try{
-//            db = openOrCreateDatabase(dbName, Activity.MODE_PRIVATE, null);
+    private void createDatabase(String dbName){
+        try{
+            db = openOrCreateDatabase(dbName, Activity.MODE_PRIVATE, null);
 //            print("데이터베이스 생성 성공");
-//        }
-//        catch(Exception e){
-//            e.printStackTrace();
+        }
+        catch(Exception e){
+            e.printStackTrace();
 //            print("데이터베이스 생성 실패");
 //            print(e.getMessage().toString());
-//        }
-//    }
-//
-//    private void createTable(String tableName){
-//        try{
-//            db.execSQL("create table if not exists "
-//                    + tableName
-//                    + " ( "
-//                    + " place text, "
-//                    + " count integer, "
-//                    + " distance integer);");
+        }
+    }
+
+    private void createTable(String tableName){
+        try{
+            db.execSQL("create table if not exists "
+                    + tableName
+                    + " ( "
+                    + " place text, "
+                    + " count integer, "
+                    + " distance integer);");
 //            print("테이블 생성 성공");
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
 //            print("테이블 생성 실패");
 //            print(e.getMessage().toString());
-//        }
-//    }
+        }
+    }
 
-//    private void insertRecord(String tableName){
-//        try {
-//            db.execSQL("insert into "
-//                    + tableName
-//                    + " ( place, count, distance ) "
-//                    + " values ( '" + place + "'" + ", '" + count +  "', '" +  count +  "' );");
+    private void insertRecord(String tableName){
+//        EditText placeInput = (EditText) findViewById(R.id.placeInput);
+//        EditText countInput = (EditText) findViewById(R.id.countInput);
+//        EditText distanceInput = (EditText) findViewById(R.id.distanceInput);
+//
+//        String place = placeInput.getText().toString();
+//        String count = countInput.getText().toString();
+//        String distance = distanceInput.getText().toString();
+        try {
+            db.execSQL("insert into "
+                    + tableName
+                    + " ( place, count, distance ) "
+                    + " values ( '" + "place" + "'" + ", '" + "count" +  "', '" +  "distance" +  "' );");
 //            print("테이블 삽입 성공");
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "성공", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
 //            print("테이블 삽입 실패");
 //            print(e.getMessage().toString());
-//        }
-//    }
+        }
+    }
 
 //    private void executeRawQuery(String tableName){
 //        try {
