cc8b95c3c6c2b3056fc8ecd93c4a48f92ad80427
sang-a <kimsa94@naver.com>
2017-05-24 15:59:41 +0900

    db

--- app/src/main/java/com/iot/mymap/MainActivity.java
+++ app/src/main/java/com/iot/mymap/MainActivity.java
@@ -3,6 +3,7 @@ package com.iot.mymap;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -12,14 +13,27 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 
-
 public class MainActivity extends AppCompatActivity
 {
+    SQLiteDatabase db;
+
+//    EditText placeInput = (EditText) findViewById(R.id.placeInput);
+//    EditText countInput = (EditText) findViewById(R.id.countInput);
+
+//    EditText distanceInput = (EditText) findViewById(R.id.distanceInput);
+//    TextView status = (TextView) findViewById(R.id.status);
+
+//    String place = placeInput.getText().toString();
+//    String count = countInput.getText().toString();
+ //   String distance = distanceInput.getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        createDatabase("MyMap");
+//        createTable("MyList");
 
         Button button = (Button) findViewById(R.id.btnMap);
         button.setOnClickListener(new View.OnClickListener() {
@@ -73,13 +87,79 @@ public class MainActivity extends AppCompatActivity
         }});
 
 
-
-
-
-
-      //  EditText distance = (EditText) findViewById(R.id.distance);
-
-
+//        Button insert = (Button) findViewById(R.id.insert);
+//        insert.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+////                        insertRecord("MyList");
+////                        executeRawQuery("MyList");
+//                    }
+//                }
+//        );
     }
+
+//    private void createDatabase(String dbName){
+//        try{
+//            db = openOrCreateDatabase(dbName, Activity.MODE_PRIVATE, null);
+//            print("데이터베이스 생성 성공");
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//            print("데이터베이스 생성 실패");
+//            print(e.getMessage().toString());
+//        }
+//    }
+//
+//    private void createTable(String tableName){
+//        try{
+//            db.execSQL("create table if not exists "
+//                    + tableName
+//                    + " ( "
+//                    + " place text, "
+//                    + " count integer, "
+//                    + " distance integer);");
+//            print("테이블 생성 성공");
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//            print("테이블 생성 실패");
+//            print(e.getMessage().toString());
+//        }
+//    }
+
+//    private void insertRecord(String tableName){
+//        try {
+//            db.execSQL("insert into "
+//                    + tableName
+//                    + " ( place, count, distance ) "
+//                    + " values ( '" + place + "'" + ", '" + count +  "', '" +  count +  "' );");
+//            print("테이블 삽입 성공");
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//            print("테이블 삽입 실패");
+//            print(e.getMessage().toString());
+//        }
+//    }
+
+//    private void executeRawQuery(String tableName){
+//        try {
+//            Cursor cursor = db.rawQuery(
+//                    "select count(*) as total from "
+//                            + tableName, null);
+//            println("getCount : " + cursor.getCount());
+//            cursor.moveToNext();
+//            print("getIng : " + cursor.getInt(0));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            print(tableName + "조회 실패");
+//            print(e.getMessage().toString());
+//        }
+//    }
+
+//    private void print(String msg){
+//        status.append("\n"+msg);
+//    }
 
 }
